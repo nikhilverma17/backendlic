a0eac4a0b554e110faddd07e651d77850c1abf89
Nikhil Verma <63592210+nikhilverma17@users.noreply.github.com>
2023-09-13 15:37:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,19 +17,19 @@ app.use(express.json());
 app.use("/api/users", userRoutes); 
 app.use("/api/notes", noteRoutes); 
 
-__dirname = path.resolve();
+// __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-    });
-} else {
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+//     });
+// } else {
     app.get("/", (req, res) => {
         res.send("API is running..");
     });
-}
+// }
 
 app.use(notFound);
 app.use(errorHandler);
